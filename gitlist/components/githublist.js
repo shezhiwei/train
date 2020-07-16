51d8935524f287e07c3cdf3c12fbb806f9ea0063
铅铅铅笔 <1197530962@qq.com>
2020-07-16 14:43:26 +0800

    build/

---
+++ gitlist/components/githublist.js
@@ -0,0 +1,40 @@
+
+import { Card, Col, Row} from 'antd'
+import React, { useContext} from 'react'
+import myContext from './mycreate'
+const List=()=>{  
+    const list=useContext(myContext) 
+    {
+        if(list!=undefined){      
+    return (             
+            <div style={{width:"100%",textAlign:"center"}}>           
+                <Row gutter={20} style={{margin:0,marginTop:"20px",display:'flex',justifyContent:'center'}}>
+                    {
+                    list.data.items.map((item,index)=><Col span={4} xs={24} sm={12} md={6} lg={4} xl={4} key={index}>
+                    <Card size="small" title={'#'+(index+1)} 
+                        cover={<img alt="example" src={item.owner.avatar_url} 
+                        />}
+                    >         
+                            <p href={item.html_url}>{item.full_name}</p>             
+                            <p><i className="fa fa-user  fa-lg" style={{ color: 'orange' }}></i> {item.owner.login}</p>
+                            <p><i className="fa fa-star fa-lg fa-fw" style={{ color: 'yellow' }}></i>{item.stargazers_count}</p>
+                            <p><i className="fa fa-code-fork fa-lg fa-fw" style={{ color: 'lightblue' }}></i>{item.forks}</p>
+                            <p><i className="fa fa-warning fa-lg fa-fw" style={{ color: 'purple' }}></i>{item.open_issues}</p>
+                        </Card>
+
+                    </Col>)
+                    }
+                    </Row>
+                </div>
+    )}else{
+        return(
+            <h2 style={{textAlign:'center'}}>Loading....</h2>
+            
+        )
+    }
+}
+                }
+
+
+
+  export default List
---
+++ gitlist/pages/_app.js
@@ -0,0 +1,4 @@
+import App from 'next/app'
+import 'antd/dist/antd.css'
+import 'font-awesome/css/font-awesome.css';
+export default App
